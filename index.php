90479c83ec7d62981a5d10a4ee23b7e8a788681f
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-08 16:30:08 +0100

    Prima prova di stampa a schermo di una istanza

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<!-- <?php  ?> -->
+
 <?php 
-  // include_once __DIR__ . "";
+  include_once __DIR__ . "/classes/item.php";
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +18,20 @@
   <title>php-oop-2</title>
 </head>
 
+<?php 
+  $item_1 = new Item (
+    "P9 Lite",
+    "Huawei",
+    "Huawei P9 Lite è un smartphone Android completo, che non ha molto da invidare ai dispositivi più avanzati. Dispone di un enorme display Touchscreen da 5.2 pollici con una risoluzione di 1920x1080 pixel. Sul versante delle funzionalità a questo Huawei P9 Lite non manca davvero nulla. A cominciare dal modulo LTE 4G che permette un trasferimento dati e una navigazione in internet eccellente, passando per la connettività Wi-fi e il GPS.",
+    "€ 139,99",
+  )
+?> 
+
 <body>
-  <h1>php-oop-2</h1>  
+  <h1>php-oop-2</h1>
+  <p><?php echo $item_1->name; ?></p>
+  <p><?php echo $item_1->brand; ?></p>
+  <p><?php echo $item_1->description; ?></p>
+  <p><?php echo $item_1->price; ?></p>
 </body>
 </html>
